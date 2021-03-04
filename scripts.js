15c0a4a6520cf5dc226a13ca36bf321cd79e49e3
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-03-03 20:44:05 -0500

    Fix bug with adding .trigger-enter-active class

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ const nav = document.querySelector('.top');
 
 function handleEnter() {
   this.classList.add('trigger-enter');
-  setTimeout(() => this.classList.add('trigger-enter-active'),150);
+  setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
   background.classList.add('open');
 
   const dropdown = this.querySelector('.dropdown');
